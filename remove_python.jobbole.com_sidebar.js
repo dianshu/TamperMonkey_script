50d4160cb2ad0c0443c5b7797ebc7c0ab8c15198
dianshu <feishicheng2015@163.com>
2018-04-17 09:40:04 +0800

    添加JQuery CDN引用

--- remove_python.jobbole.com_sidebar.js
+++ remove_python.jobbole.com_sidebar.js
@@ -6,12 +6,13 @@
 // @author       feishicheng
 // @match        http://*.jobbole.com/*
 // @grant        none
+// @require      http://code.jquery.com/jquery-3.3.1.min.js
 // ==/UserScript==
 
 (function() {
     'use strict';
     // 去除右侧边栏标签
-    jQuery("#sidebar").remove();
+    $("#sidebar").remove();
     // 设置左侧内容宽度
     var newWidth = jQuery(".header-wrapper").css("width");
     jQuery("div[id^='post-']").css("width", newWidth);
